d121c8fe7d1b027de33b83e189a126d4e44a53c8
amisadman <inboxsadman@gmail.com>
2026-06-27 12:39:11 +0600

    feat: added parking zones repository layer

---
+++ internal/domain/parkingzone/repository.go
@@ -0,0 +1,41 @@
+package parkingzone
+
+import (
+	"gorm.io/gorm"
+)
+
+type Repository interface {
+	CreateZone(zone *ParkingZone) error
+	GetAllZones() ([]ParkingZone, error)
+	GetZoneByID(id uint) (*ParkingZone, error)
+}
+
+type parkingZoneRepository struct {
+	db *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) Repository {
+	return &parkingZoneRepository{db: db}
+}
+
+func (r *parkingZoneRepository) CreateZone(zone *ParkingZone) error {
+	return r.db.Create(zone).Error
+}
+
+func (r *parkingZoneRepository) GetAllZones() ([]ParkingZone, error) {
+	var zones []ParkingZone
+	err := r.db.Find(&zones).Error
+	return zones, err
+}
+
+func (r *parkingZoneRepository) GetZoneByID(id uint) (*ParkingZone, error) {
+	var zone ParkingZone
+	err := r.db.First(&zone, id).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &zone, nil
+}
