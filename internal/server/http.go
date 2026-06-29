9876e8ea7f47b16eff172f9dca4b090412ad37b1
amisadman <inboxsadman@gmail.com>
2026-06-29 22:35:14 +0600

    feat: added root welcome endpoint

--- internal/server/http.go
+++ internal/server/http.go
@@ -1,17 +1,27 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"runtime"
 	"spotsync/internal/config"
 	"spotsync/internal/domain/parkingzone"
 	"spotsync/internal/domain/reservation"
 	"spotsync/internal/domain/user"
+	"time"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v5"
 	"github.com/labstack/echo/v5/middleware"
 	"gorm.io/gorm"
 )
+
+var startTime time.Time
+
+func init() {
+	startTime = time.Now()
+}
 
 type CustomValidator struct {
 	validator *validator.Validate
@@ -30,6 +40,55 @@ func Start(db *gorm.DB, cfg *config.Config){
 	e.Use(middleware.RequestLogger())
 	e.Use(middleware.CORS())
 	
+	e.GET("/", func(c *echo.Context) error {
+		hostname, err := os.Hostname()
+		if err != nil {
+			hostname = "unknown"
+		}
+
+		duration := time.Since(startTime)
+		hours := int(duration.Hours())
+		minutes := int(duration.Minutes()) % 60
+		uptimeStr := fmt.Sprintf("%d hours %d minutes", hours, minutes)
+
+		clientIP := c.RealIP()
+		accessedAt := time.Now().UTC().Format("2006-01-02T15:04:05.000Z")
+
+		type ClientDetails struct {
+			ClientIP   string `json:"clientIP"`
+			AccessedAt string `json:"accessedAt"`
+		}
+
+		type ServerDetails struct {
+			Hostname string `json:"hostname"`
+			Platform string `json:"platform"`
+			Uptime   string `json:"uptime"`
+		}
+
+		type WelcomeResponse struct {
+			Success       bool          `json:"success"`
+			Message       string        `json:"message"`
+			Version       string        `json:"version"`
+			ClientDetails ClientDetails `json:"clientDetails"`
+			ServerDetails ServerDetails `json:"serverDetails"`
+		}
+
+		return c.JSON(http.StatusOK, WelcomeResponse{
+			Success: true,
+			Message: "Welcome to Spotsync",
+			Version: "1.0.0",
+			ClientDetails: ClientDetails{
+				ClientIP:   clientIP,
+				AccessedAt: accessedAt,
+			},
+			ServerDetails: ServerDetails{
+				Hostname: hostname,
+				Platform: runtime.GOOS,
+				Uptime:   uptimeStr,
+			},
+		})
+	})
+
 	e.GET("/health", func(c *echo.Context) error {
 		return c.JSON(http.StatusOK, map[string]string{
 			"status": "ok",
